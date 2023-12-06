333c622331bd942af12aa2a452b24bba415e07a7
Skarcher29 <samanthakarcher@gmail.com>
2023-12-05 22:32:49 -0600

    initial commit

---
+++ Classes/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        /*Make string public
+        Model string public
+        Year int public
+        */
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car()
+            {
+                Make = "Toyota",
+                Model = "Corolla",
+                Year = 2003
+            };
+
+            Console.WriteLine("This is the Make, Model, & Year of my car:");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine($"{myCar.Make} {myCar.Model} {myCar.Year}");
         }
     }
 }
